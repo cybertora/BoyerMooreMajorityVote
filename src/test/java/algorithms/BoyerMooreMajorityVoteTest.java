7cbf36372de9e86ab60ec95e905702609f8e5d84
rassul <rtokatov@gmail.com>
2025-10-02 00:22:33 +0500

    feature/new testing

--- src/test/java/algorithms/BoyerMooreMajorityVoteTest.java
+++ src/test/java/algorithms/BoyerMooreMajorityVoteTest.java
@@ -3,25 +3,47 @@ package algorithms;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class BoyerMooreMajorityVoteTest {
+class BoyerMooreMajorityTest {
 
     @Test
-    void testMajorityElement() {
-        assertEquals(3, BoyerMooreMajorityVote.findMajorityElement(new int[]{3, 3, 4, 2, 4, 4, 2, 4, 4}));
-    }
-
-    @Test
-    void testNoMajorityElement() {
-        assertEquals(-1, BoyerMooreMajorityVote.findMajorityElement(new int[]{3, 3, 4, 2, 4, 4, 2, 4}));
-    }
-
-    @Test
-    void testSingleElementArray() {
-        assertEquals(1, BoyerMooreMajorityVote.findMajorityElement(new int[]{1}));
-    }
-
-    @Test
-    void testEmptyArray() {
+    void testEmptyArray() { // Тест пустого массива.
         assertEquals(-1, BoyerMooreMajorityVote.findMajorityElement(new int[]{}));
+    }
+
+
+    @Test
+    void testSingleElement() {
+        assertEquals(5, BoyerMooreMajorityVote.findMajorityElement(new int[]{5}));
+    }
+
+    @Test
+    void testMajorityExists() {
+        assertEquals(2, BoyerMooreMajorityVote.findMajorityElement(new int[]{2, 2, 1}));
+        assertEquals(3, BoyerMooreMajorityVote.findMajorityElement(new int[]{3, 2, 3}));
+    }
+
+    @Test
+    void testNoMajority() {
+        assertEquals(-1, BoyerMooreMajorityVote.findMajorityElement(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    void testDuplicates() {
+        assertEquals(1, BoyerMooreMajorityVote.findMajorityElement(new int[]{1,1,1,1}));
+    }
+
+    @Test
+    void testEvenLengthNoMajority() {
+        assertEquals(-1, BoyerMooreMajorityVote.findMajorityElement(new int[]{1,2,1,2}));
+    }
+
+    @Test
+    void testSortedArray() { // Отсортированный с большинством.
+        assertEquals(2, BoyerMooreMajorityVote.findMajorityElement(new int[]{1,2,2,2,3}));
+    }
+
+    @Test
+    void testReverseSorted() {
+        assertEquals(2, BoyerMooreMajorityVote.findMajorityElement(new int[]{3,2,2,2,1}));
     }
 }
