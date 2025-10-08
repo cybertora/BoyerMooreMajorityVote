5ab9aa7258c3f6f2159bf07e9939a7e8e23395e2
rassul <rtokatov@gmail.com>
2025-10-08 17:11:15 +0500

    feat/testing

---
+++ src/test/java/algorithms/BoyerMooreMajorityTest.java
@@ -0,0 +1,57 @@
+package algorithms;
+
+import metrics.MetricsTracker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoyerMooreMajorityTest {
+
+    private MetricsTracker tracker;
+
+    @BeforeEach
+    void setUp() {
+        tracker = new MetricsTracker(); // Инициализация перед каждым тестом
+    }
+
+    @Test
+    void testEmptyArray() {
+        assertEquals(-1, BoyerMooreMajorityVote.findMajorityElement(new int[]{}, tracker));
+    }
+
+    @Test
+    void testSingleElement() {
+        assertEquals(5, BoyerMooreMajorityVote.findMajorityElement(new int[]{5}, tracker));
+    }
+
+    @Test
+    void testMajorityExists() {
+        assertEquals(2, BoyerMooreMajorityVote.findMajorityElement(new int[]{2, 2, 1}, tracker));
+        assertEquals(3, BoyerMooreMajorityVote.findMajorityElement(new int[]{3, 2, 3}, tracker));
+    }
+
+    @Test
+    void testNoMajority() {
+        assertEquals(-1, BoyerMooreMajorityVote.findMajorityElement(new int[]{1, 2, 3}, tracker));
+    }
+
+    @Test
+    void testDuplicates() {
+        assertEquals(1, BoyerMooreMajorityVote.findMajorityElement(new int[]{1, 1, 1, 1}, tracker));
+    }
+
+    @Test
+    void testEvenLengthNoMajority() {
+        assertEquals(-1, BoyerMooreMajorityVote.findMajorityElement(new int[]{1, 2, 1, 2}, tracker));
+    }
+
+    @Test
+    void testSortedArray() {
+        assertEquals(2, BoyerMooreMajorityVote.findMajorityElement(new int[]{1, 2, 2, 2, 3}, tracker));
+    }
+
+    @Test
+    void testReverseSorted() {
+        assertEquals(2, BoyerMooreMajorityVote.findMajorityElement(new int[]{3, 2, 2, 2, 1}, tracker));
+    }
+}
